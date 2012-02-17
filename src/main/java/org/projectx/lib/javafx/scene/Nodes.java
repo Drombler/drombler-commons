9f280217a3969be57bfa950f8974395daa6af9e1
puce <puce@users.sourceforge.net>
2012-02-17 20:48:27 +0100

    - moved Nodes

---
+++ src/main/java/org/projectx/lib/javafx/scene/Nodes.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.projectx.lib.javafx.scene;
+
+import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Window;
+
+/**
+ *
+ * @author puce
+ */
+public class Nodes {
+
+    private Nodes() {
+    }
+
+    public static Point2D getScreenLocation(Node node) {
+        Bounds boundsInLocal = node.getBoundsInLocal(); // TODO: correct???
+        Scene scene = node.getScene();
+        Window window = scene.getWindow();
+        double x = window.getX() + scene.getX() + boundsInLocal.getMinX() + 300;
+        double y = window.getY() + scene.getY() + boundsInLocal.getMinY() + 250;
+        return new Point2D(x, y);
+    }
+}
