253332aea5fa74c7ea6ed75bb16a889c5d34cc8d
puce <puce@users.sourceforge.net>
2012-12-12 00:27:39 +0100

    - FixedBooleanProperty added

---
+++ drombler-commons-fx/src/main/java/org/drombler/commons/fx/beans/property/FixedBooleanProperty.java
@@ -0,0 +1,49 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is Drombler.org. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce).
+ * Copyright 2012 Drombler.org. All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+package org.drombler.commons.fx.beans.property;
+
+import javafx.beans.property.ReadOnlyBooleanPropertyBase;
+
+/**
+ * TODO: useful?
+ * @author puce
+ */
+public class FixedBooleanProperty extends ReadOnlyBooleanPropertyBase {
+    private final Object bean;
+    private final String name;
+    private final boolean value;
+
+    public FixedBooleanProperty(Object bean, String name, boolean value) {
+        this.bean = bean;
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public final boolean get() {
+        return value;
+    }
+
+    @Override
+    public Object getBean() {
+        return bean;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+}
