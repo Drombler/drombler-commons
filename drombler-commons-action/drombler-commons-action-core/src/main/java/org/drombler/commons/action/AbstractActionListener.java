7d86a9c5d8652fbadafee4811a86a79d0d28228f
puce <puce@users.sourceforge.net>
2014-06-15 13:35:37 +0200

    [#46] Make the Action Framework usable outside Drombler FX - initial migration

---
+++ drombler-commons-action/drombler-commons-action-core/src/main/java/org/drombler/commons/action/AbstractActionListener.java
@@ -0,0 +1,65 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is Drombler.org. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce).
+ * Copyright 2012 Drombler.org. All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+package org.drombler.commons.action;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ *
+ * @author puce
+ */
+public abstract class AbstractActionListener<E> implements ActionListener<E> {
+
+    private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+    private boolean disabled = false;
+
+    @Override
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    protected void setDisabled(boolean disabled) {
+        if (this.disabled != disabled) {
+            boolean oldValue = this.disabled;
+            this.disabled = disabled;
+            getPropertyChangeSupport().firePropertyChange("disabled", oldValue, disabled);
+        }
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        getPropertyChangeSupport().addPropertyChangeListener(listener);
+    }
+
+    @Override
+    public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+        getPropertyChangeSupport().addPropertyChangeListener(propertyName, listener);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        getPropertyChangeSupport().removePropertyChangeListener(listener);
+    }
+
+    @Override
+    public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+        getPropertyChangeSupport().removePropertyChangeListener(propertyName, listener);
+    }
+
+    protected PropertyChangeSupport getPropertyChangeSupport() {
+        return propertyChangeSupport;
+    }
+}
---
+++ drombler-commons-action/drombler-commons-action-core/src/main/java/org/drombler/commons/action/AbstractToggleActionListener.java
@@ -0,0 +1,44 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is Drombler.org. The Initial Developer of the
+ * Original Code is Florian Brunner (Sourceforge.net user: puce).
+ * Copyright 2012 Drombler.org. All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
+package org.drombler.commons.action;
+
+/**
+ *
+ * @author puce
+ */
+public abstract class AbstractToggleActionListener<E> extends AbstractActionListener<E> implements ToggleActionListener<E> {
+
+    private boolean selected = false;
+
+    @Override
+    public boolean isSelected() {
+        return selected;
+    }
+
+    @Override
+    public void setSelected(boolean selected) {
+        if (this.selected != selected) {
+            boolean oldValue = this.selected;
+            this.selected = selected;
+            onSelectionChanged(oldValue, selected);
+            getPropertyChangeSupport().firePropertyChange("selected", oldValue, selected);
+        }
+    }
+
+    @Override
+    public void onAction(E event) {
+        // do nothing
+    }
+}
