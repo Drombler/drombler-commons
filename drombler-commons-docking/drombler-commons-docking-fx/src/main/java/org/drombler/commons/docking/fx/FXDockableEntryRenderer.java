30109b44561867bf4a8f423a223ef3789873e684
Florian Brunner <fbrunnerreg@gmx.ch>
2016-06-15 00:11:38 +0200

    #13 DockingPane: make requestActive-method work - DockingPane.activeDockable: select tab - Javadoc

--- drombler-commons-docking/drombler-commons-docking-fx/src/main/java/org/drombler/commons/docking/fx/FXDockableEntryRenderer.java
+++ drombler-commons-docking/drombler-commons-docking-fx/src/main/java/org/drombler/commons/docking/fx/FXDockableEntryRenderer.java
@@ -3,8 +3,10 @@ package org.drombler.commons.docking.fx;
 import javafx.scene.Node;
 import javafx.scene.control.Tooltip;
 import org.drombler.commons.fx.scene.renderer.AbstractDataRenderer;
+import org.drombler.commons.fx.scene.renderer.DataRenderer;
 
 /**
+ * A {@link DataRenderer} for {@link FXDockableEntry}.
  *
  * @author puce
  */
@@ -12,20 +14,34 @@ public class FXDockableEntryRenderer extends AbstractDataRenderer<FXDockableEntr
 
     private final int graphicSize;
 
+    /**
+     * Creates a new instance of this class.
+     *
+     * @param graphicSize the size of the graphic
+     */
     public FXDockableEntryRenderer(int graphicSize) {
         this.graphicSize = graphicSize;
     }
 
+    /**
+     * {@inheritDoc }
+     */
     @Override
     public String getText(FXDockableEntry item) {
         return item.getDockableData().getTitle();
     }
 
+    /**
+     * {@inheritDoc }
+     */
     @Override
     public Node getGraphic(FXDockableEntry item) {
         return item.getDockableData().getGraphicFactory().createGraphic(graphicSize); // TODO: avoid creating new graphic nodes (dockableData.getGraphic() cannot be used since it already has a parent (Tab))
     }
 
+    /**
+     * {@inheritDoc }
+     */
     @Override
     public Tooltip getTooltip(FXDockableEntry item) {
         return item.getDockableData().getTooltip();
