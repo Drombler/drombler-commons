ec838ffb4de3a4a71f1a052b7c38416ec146416e
Florian Brunner <fbrunnerreg@gmx.ch>
2016-09-22 01:21:21 +0200

    #111 File Type Editor Registration - bound properties

--- drombler-commons-docking/drombler-commons-docking-context/src/test/java/org/drombler/commons/docking/context/impl/TestDockableData.java
+++ drombler-commons-docking/drombler-commons-docking-context/src/test/java/org/drombler/commons/docking/context/impl/TestDockableData.java
@@ -1,17 +1,18 @@
 package org.drombler.commons.docking.context.impl;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import org.drombler.commons.docking.DockableData;
 
 /**
  *
  * @author puce
  */
-
-
 public class TestDockableData implements DockableData {
 
     private String title;
     private boolean modified;
+    private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
     @Override
     public String getTitle() {
@@ -20,7 +21,9 @@ public class TestDockableData implements DockableData {
 
     @Override
     public void setTitle(String title) {
+        String oldTitle = this.title;
         this.title = title;
+        propertyChangeSupport.firePropertyChange(TITLE_PROPERTY_NAME, oldTitle, this.title);
     }
 
     @Override
@@ -30,7 +33,19 @@ public class TestDockableData implements DockableData {
 
     @Override
     public void setModified(boolean modified) {
+        boolean oldModified = this.modified;
         this.modified = modified;
+        propertyChangeSupport.firePropertyChange(MODIFIED_PROPERTY_NAME, oldModified, this.modified);
+    }
+
+    @Override
+    public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
+    }
+
+    @Override
+    public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(propertyName, listener);
     }
 
 }
