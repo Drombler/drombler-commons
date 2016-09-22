3d856cb1876f6022d2b0895c8efcfc0e3e9a71d4
Florian Brunner <fbrunnerreg@gmx.ch>
2016-09-22 21:31:44 +0200

    #111 File Type Editor Registration - Javadoc fix

--- drombler-commons-data/src/main/java/org/drombler/commons/data/DataHandler.java
+++ drombler-commons-data/src/main/java/org/drombler/commons/data/DataHandler.java
@@ -8,9 +8,7 @@ import org.softsmithy.lib.util.UniqueKeyProvider;
  *
  * To fully integrate with the framework it should observe registered {@link DataCapabilityProvider}s and add the found data capabilities to it's local context.
  *
- * The easiest way to implement a data handler is to extend {@link AbstractDataHandler}, but it's not required.
  *
- * @see AbstractDataHandler
  * @param <T> the type of the unique key of this data handler
  * @author puce
  */
