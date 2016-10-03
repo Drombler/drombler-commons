3703d65e1b6cb980d05b56859456033866403b4e
Florian Brunner <fbrunnerreg@gmx.ch>
2016-10-03 22:27:36 +0200

    #111 File Type Editor Registration - file headers - clean-up

--- drombler-commons-data/src/main/java/org/drombler/commons/data/DataCapabilityProvider.java
+++ drombler-commons-data/src/main/java/org/drombler/commons/data/DataCapabilityProvider.java
@@ -1,3 +1,17 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is Drombler.org. The Initial Developer of the
+ * Original Code is Florian Brunner (GitHub user: puce77).
+ * Copyright 2016 Drombler.org. All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
 package org.drombler.commons.data;
 
 /**
@@ -5,8 +19,11 @@ package org.drombler.commons.data;
  *
  * This class can be used to register generic data capabilities to data handlers. Eg. {@link Openable} is a generic data capability which provides a mechanism to open a data handler in an editor.
  *
+ * Note: This interface might change in future releases.
+ *
  * @param <T> the data capability type
  * @see Openable
+ * @see DataHandler
  * @author puce
  */
 // * @see DocumentHandler
--- drombler-commons-data/src/main/java/org/drombler/commons/data/DataHandlerDescriptorRegistry.java
+++ drombler-commons-data/src/main/java/org/drombler/commons/data/DataHandlerDescriptorRegistry.java
@@ -1,15 +1,63 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is Drombler.org. The Initial Developer of the
+ * Original Code is Florian Brunner (GitHub user: puce77).
+ * Copyright 2016 Drombler.org. All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
 package org.drombler.commons.data;
 
-public interface DataHandlerDescriptorRegistry {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-    void registerDataHandlerDescriptor(AbstractDataHandlerDescriptor<?> dataHandlerDescriptor);
+/**
+ * A registry for Document Handler descriptors.
+ *
+ * @author puce
+ */
+public class DataHandlerDescriptorRegistry {
 
-    void unregisterDataHandlerDescriptor(AbstractDataHandlerDescriptor<?> dataHandlerDescriptor);
+    private final Map<Class<?>, AbstractDataHandlerDescriptor<?>> documentHandlerClasses = new HashMap<>();
+    private final Set<DataHandlerDescriptorListener> listeners = new HashSet<>();
 
-    AbstractDataHandlerDescriptor<?> getDataHandlerDescriptor(Object dataHandler);
+    public void registerDataHandlerDescriptor(AbstractDataHandlerDescriptor<?> dataHandlerDescriptor) {
+        documentHandlerClasses.put(dataHandlerDescriptor.getDataHandlerClass(), dataHandlerDescriptor);
+        fireDataHandlerDescriptorAdded(dataHandlerDescriptor);
+    }
 
-    void registerDataHandlerDescriptorListener(DataHandlerDescriptorListener listener);
+    public void unregisterDataHandlerDescriptor(AbstractDataHandlerDescriptor<?> dataHandlerDescriptor) {
+        documentHandlerClasses.remove(dataHandlerDescriptor.getDataHandlerClass(), dataHandlerDescriptor);
+        fireDataHandlerDescriptorRemoved(dataHandlerDescriptor);
+    }
 
-    void unregisterDataHandlerDescriptorListener(DataHandlerDescriptorListener listener);
+    public AbstractDataHandlerDescriptor<?> getDataHandlerDescriptor(Object dataHandler) {
+        return documentHandlerClasses.get(dataHandler.getClass());
+    }
 
+    public void registerDataHandlerDescriptorListener(DataHandlerDescriptorListener listener) {
+        listeners.add(listener);
+    }
+
+    public void unregisterDataHandlerDescriptorListener(DataHandlerDescriptorListener listener) {
+        listeners.remove(listener);
+    }
+
+    private <D> void fireDataHandlerDescriptorAdded(AbstractDataHandlerDescriptor<D> dataHandlerDescriptor) {
+        DataHandlerDescriptorEvent<D> event = new DataHandlerDescriptorEvent<>(this, dataHandlerDescriptor);
+        listeners.forEach(listener -> listener.dataHandlerDescriptorAdded(event));
+    }
+
+    private <D> void fireDataHandlerDescriptorRemoved(AbstractDataHandlerDescriptor<D> dataHandlerDescriptor) {
+        DataHandlerDescriptorEvent<D> event = new DataHandlerDescriptorEvent<>(this, dataHandlerDescriptor);
+        listeners.forEach(listener -> listener.dataHandlerDescriptorRemoved(event));
+    }
 }
