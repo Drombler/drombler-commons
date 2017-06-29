724e1aa55f1506b71d79532927a96daa4b859606
Florian Brunner <fbrunnerreg@gmx.ch>
2017-06-29 20:04:03 +0200

    #170 Provide a progress monitor - Popup

--- drombler-commons-fx/drombler-commons-fx-core/src/main/java/org/drombler/commons/fx/scene/control/impl/skin/ProgressMonitorSkin.java
+++ drombler-commons-fx/drombler-commons-fx-core/src/main/java/org/drombler/commons/fx/scene/control/impl/skin/ProgressMonitorSkin.java
@@ -2,8 +2,9 @@ package org.drombler.commons.fx.scene.control.impl.skin;
 
 import javafx.beans.binding.Bindings;
 import javafx.geometry.Point2D;
-import javafx.scene.control.PopupControl;
+import javafx.scene.control.ListView;
 import javafx.scene.control.SkinBase;
+import javafx.stage.Popup;
 import javafx.stage.PopupWindow;
 import org.drombler.commons.fx.scene.Nodes;
 import org.drombler.commons.fx.scene.control.ProgressMonitor;
@@ -12,8 +13,6 @@ import org.drombler.commons.fx.scene.control.ProgressMonitor;
  *
  * @author puce
  */
-
-
 public class ProgressMonitorSkin extends SkinBase<ProgressMonitor> {
 
     private final ProgressMonitorContentPane contentPane = new ProgressMonitorContentPane();
@@ -33,8 +32,9 @@ public class ProgressMonitorSkin extends SkinBase<ProgressMonitor> {
 
     private void displayAllTasks() {
         // TODO: check if hide instead of display?
-        PopupControl tasksPopup = new PopupControl();
-//        tasksPopup.getContent().add(new ListView(getSkinnable().getTasks()));
+//        PopupControl tasksPopup = new PopupControl();
+        Popup tasksPopup = new Popup();
+        tasksPopup.getContent().add(new ListView(getSkinnable().getTasks()));
         tasksPopup.setAutoHide(true);
         tasksPopup.setAnchorLocation(PopupWindow.AnchorLocation.WINDOW_BOTTOM_LEFT);
         Point2D screenLocation = Nodes.getScreenLocation(contentPane);
