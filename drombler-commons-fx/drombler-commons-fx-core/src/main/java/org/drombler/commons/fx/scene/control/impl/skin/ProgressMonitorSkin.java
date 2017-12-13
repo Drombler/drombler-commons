66a55ad7f7406215d4daf68bb08db71b65656ef2
Florian Brunner <fbrunnerreg@gmx.ch>
2017-12-13 22:22:00 +0100

    #170 Provide a progress monitor - clean-up

--- drombler-commons-fx/drombler-commons-fx-core/src/main/java/org/drombler/commons/fx/scene/control/impl/skin/ProgressMonitorSkin.java
+++ drombler-commons-fx/drombler-commons-fx-core/src/main/java/org/drombler/commons/fx/scene/control/impl/skin/ProgressMonitorSkin.java
@@ -1,3 +1,17 @@
+/*
+ *         COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Notice
+ *
+ * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.opensource.org/licenses/cddl1.txt
+ *
+ * The Original Code is Drombler.org. The Initial Developer of the
+ * Original Code is Florian Brunner (GitHub user: puce77).
+ * Copyright 2017 Drombler.org. All Rights Reserved.
+ *
+ * Contributor(s): .
+ */
 package org.drombler.commons.fx.scene.control.impl.skin;
 
 import javafx.beans.binding.Bindings;
@@ -17,7 +31,6 @@ public class ProgressMonitorSkin extends SkinBase<ProgressMonitor> {
 
     private final ProgressMonitorContentPane contentPane = new ProgressMonitorContentPane();
     private final WorkersPopup workersPopup = new WorkersPopup();
-//    private final ProgressMonitorPopupContentPane popupContentPane = new ProgressMonitorPopupContentPane();
     private final BooleanBinding workersEmptyBinding = Bindings.isEmpty(workersPopup.getWorkers());
 
     /**
@@ -33,8 +46,7 @@ public class ProgressMonitorSkin extends SkinBase<ProgressMonitor> {
         contentPane.managedProperty().bind(contentPane.visibleProperty());
         contentPane.setOnMouseClicked(event -> displayAllTasks());
 
-//        workersPopup = createPopup();
-        createPopup();
+        initWorkersPopup();
     }
 
     private void displayAllTasks() {
@@ -47,66 +59,25 @@ public class ProgressMonitorSkin extends SkinBase<ProgressMonitor> {
         }
     }
 
-    private void createPopup() {
-        // TODO: check if hide instead of display?
-//        PopupControl workersPopup = new PopupControl();
-
+    private void initWorkersPopup() {
         Bindings.bindContent(workersPopup.getWorkers(), getSkinnable().getWorkers());
         workersPopup.mainWorkerProperty().bind(getSkinnable().mainWorkerProperty());
-//        Bindings.isEmpty(popupContentPane.getWorkers()).and(popupContentPane.focusedProperty()).addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
-//            getSkinnable()
-//        });
-
-//        WorkersPopup popup = new WorkersPopup();// {
-        WorkersPopup popup = this.workersPopup;
-
-
-//            @Override
-//            public Styleable getStyleableParent() {
-//                return ProgressMonitorSkin.this.getSkinnable();
-//            }
-
-//            {
-//        popup.setSkin(new Skin<Skinnable>() {
-//                    @Override
-//                    public Skinnable getSkinnable() {
-//                        return popup;
-//                    }
-//
-//                    @Override
-//                    public Node getNode() {
-//                        return popupContentPane;
-//                    }
-//
-//                    @Override
-//                    public void dispose() {
-//                    }
-//                });
-//            }
-//        };
 
         getSkinnable().sceneProperty().addListener(o -> {
             if (((ObservableValue) o).getValue() == null) {
-                if (popup.isShowing()) {
-                    popup.hide();
+                if (workersPopup.isShowing()) {
+                    workersPopup.hide();
                 }
             }
         });
         workersEmptyBinding.addListener((observable, oldValue, newValue) -> {
-            if (popup.isShowing()) {
-                popup.hide();
+            if (workersPopup.isShowing()) {
+                workersPopup.hide();
             }
         });
 
-//        popup.getScene().setRoot(popupContentPane);
-//        popup.getScene().setUserAgentStylesheet(getSkinnable().getUserAgentStylesheet());
-        popup.setAutoHide(true);
-        popup.setAnchorLocation(PopupWindow.AnchorLocation.WINDOW_BOTTOM_LEFT);
-//        popup.getScene().setFill(Color.LIGHTBLUE);
-//        popup.setStyle("-fx-background-color: green;");//-fx-border-color: black; -fx-border-width: 1px;");
-        popup.getStyleClass().add("progress-monitor-popup");
-
-//        return popup;
+        workersPopup.setAutoHide(true);
+        workersPopup.setAnchorLocation(PopupWindow.AnchorLocation.WINDOW_BOTTOM_LEFT);
     }
 
 }
