53cd16ef738488694f4d98c2ac989fdada3545f7
Florian Brunner <fbrunnerreg@gmx.ch>
2018-01-05 19:14:42 +0100

    AbstractFXToggleAction added

---
+++ drombler-commons-action/drombler-commons-action-fx/src/main/java/org/drombler/commons/action/fx/AbstractFXToggleAction.java
@@ -0,0 +1,42 @@
+package org.drombler.commons.action.fx;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+
+/**
+ *
+ * @author puce
+ */
+
+
+public class AbstractFXToggleAction extends AbstractFXAction implements FXToggleAction {
+
+    private final BooleanProperty selected = new SimpleBooleanProperty(this, "selected");
+
+
+    @Override
+    public final boolean isSelected() {
+        return selectedProperty().get();
+    }
+
+    @Override
+    public final void setSelected(boolean selected) {
+        selectedProperty().set(selected);
+    }
+
+    @Override
+    public BooleanProperty selectedProperty() {
+        return selected;
+    }
+
+    /**
+     * Does nothing by default.
+     *
+     * @param event the action event
+     */
+    @Override
+    public void handle(ActionEvent event) {
+        // do nothing
+    }
+}
