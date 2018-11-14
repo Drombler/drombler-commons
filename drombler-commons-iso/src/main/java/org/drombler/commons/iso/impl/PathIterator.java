5ce4e1874670bf57f0d880e5f4fb6e4d8fdbc3a4
Florian Brunner <fbrunnerreg@gmx.ch>
2018-11-14 23:35:02 +0100

    - URL fix - clean-up

--- drombler-commons-iso/src/main/java/org/drombler/commons/iso/impl/PathIterator.java
+++ drombler-commons-iso/src/main/java/org/drombler/commons/iso/impl/PathIterator.java
@@ -4,7 +4,7 @@
  * The contents of this file are subject to the COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL)
  * Version 1.0 (the "License"); you may not use this file except in
  * compliance with the License. A copy of the License is available at
- * http://www.opensource.org/licenses/cddl1.txt
+ * https://www.opensource.org/licenses/cddl1.txt
  *
  * The Original Code is Drombler.org. The Initial Developer of the
  * Original Code is Florian Brunner (Sourceforge.net user: puce).
@@ -18,11 +18,7 @@ import java.nio.file.Path;
 import java.util.Iterator;
 
 /**
-<<<<<<< OURS
  * TODO: Move to SoftSmity nio.file.spi?
-=======
- * TODO: Move to SoftSmity?
->>>>>>> THEIRS
  *
  * @author puce
  */
