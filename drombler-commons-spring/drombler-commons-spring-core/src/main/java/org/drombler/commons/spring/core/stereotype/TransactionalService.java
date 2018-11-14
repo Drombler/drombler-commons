fec0c5bf4db695aee442a4a38d3805e27a26cd94
Florian Brunner <fbrunnerreg@gmx.ch>
2018-11-14 23:06:26 +0100

    - fixes - clean-up

--- drombler-commons-spring/drombler-commons-spring-core/src/main/java/org/drombler/commons/spring/core/stereotype/TransactionalService.java
+++ drombler-commons-spring/drombler-commons-spring-core/src/main/java/org/drombler/commons/spring/core/stereotype/TransactionalService.java
@@ -2,10 +2,12 @@ package org.drombler.commons.spring.core.stereotype;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.softsmithy.lib.util.BusinessException;
+import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,11 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
  *
  * @author puce
  */
-@Target(value = {ElementType.TYPE})
-@Retention(value = RetentionPolicy.RUNTIME)
+@Inherited
 @Documented
-@Transactional(rollbackFor = BusinessException.class)
+@Retention(value = RetentionPolicy.RUNTIME)
 @Service
+@Transactional(rollbackFor = BusinessException.class)
+@Target(value = {ElementType.TYPE})
 public @interface TransactionalService {
-    
+
+    /**
+     * The suggested component name or else empty String.
+     *
+     * @return the suggested component name or else empty String
+     */
+    @AliasFor(annotation = Service.class)
+    String value() default "";
 }
