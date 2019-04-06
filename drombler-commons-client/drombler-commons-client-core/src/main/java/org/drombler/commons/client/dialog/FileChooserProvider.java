917e27bb571dec718987235db9e1eacc7ed01598
Florian Brunner <fbrunnerreg@gmx.ch>
2019-04-06 22:59:45 +0200

    Javadoc

--- drombler-commons-client/drombler-commons-client-core/src/main/java/org/drombler/commons/client/dialog/FileChooserProvider.java
+++ drombler-commons-client/drombler-commons-client-core/src/main/java/org/drombler/commons/client/dialog/FileChooserProvider.java
@@ -10,9 +10,25 @@ import java.util.List;
  */
 public interface FileChooserProvider {
 
+    /**
+     * Shows a file chooser dialog to select multiple files to open.
+     *
+     * @return the paths of the selected files or null if no files have been selected
+     */
     List<Path> showOpenMultipleDialog();
 
+    /**
+     * Shows a file chooser dialog to select a file to open.
+     *
+     * @return the path of the selected file or null if no file has been selected
+     */
     Path showOpenDialog();
 
+    /**
+     * Shows a file chooser dialog to choose a path to save a file.
+     *
+     * @param initialFileName the inital final name proposed for new files
+     * @return the path of the saved file or null if no file has been selected
+     */
     Path showSaveAsDialog(String initialFileName);
 }
